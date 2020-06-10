15c4ef4ae5ef04e470ae7c33d7193b2c7ed00e9d
Rustam Sarin <lorienas@gmail.com>
2020-06-10 18:42:38 +0300

    added new setup for webpack-dev-server and create app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import WelcomeScreen from "@components/welcome-screen/welcome-screen.jsx";
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+
   return (
-    <WelcomeScreen />
+    <WelcomeScreen errorsCount={errorsCount} />
   );
 };
 
